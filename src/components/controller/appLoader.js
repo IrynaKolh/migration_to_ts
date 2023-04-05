4d456266d12ef257e3fa3810e4a655dacdd7cbd5
Iryna <iryna.kolhanova@gmail.com>
2023-04-05 18:51:25 -0400

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'edd2ef8b3deb48ac858e8c510570de81', // получите свой ключ https://newsapi.org/
         });
     }
 }
