a1b5ce520246767c4105788387e093657525de62
Iryna <iryna.kolhanova@gmail.com>
2023-04-18 13:38:02 -0400

    migrate sources js to ts

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -19,3 +19,13 @@ type Sourse = {
     id: string;
     name: string;
 };
+
+export interface SourseResponse {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
